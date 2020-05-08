dd733259be32433bf2ef66acc2ffc4f4bacdf913
shatcung <33551718+shatcung@users.noreply.github.com>
2020-05-08 15:27:41 +0300

    Update admin.py

--- Modulka/admin.py
+++ Modulka/admin.py
@@ -6,12 +6,19 @@ from .models import Post, Comment
 class PostAdmin(admin.ModelAdmin):
 	
     list_display = ('post_title',
+		    
 		       'post_status','post_type','publish','post_modified')
+
     list_filter = ('post_status', 'publish')
+	
     search_fields = ('post_title', 'post_content')
+
     prepopulated_fields = {'slug': ('post_title',)}
+	
     raw_id_fields = ('post_author',)
+
     date_hierarchy = 'publish'
+	
     ordering = ('post_status', 'publish')
 
 
@@ -20,5 +27,7 @@ class PostAdmin(admin.ModelAdmin):
 class CommentAdmin(admin.ModelAdmin): 
 	
     list_display = ('name', 'email', 'post', 'created', 'active') 
+
     list_filter = ('active', 'created', 'updated') 
+	
     search_fields = ('name', 'email', 'body') 
